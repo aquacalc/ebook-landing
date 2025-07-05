a1fb5619174460b7442f26f6863e802d01d668f3
aquacalc <aquacalc@yahoo.com>
2025-07-05 12:16:43 -0400

    Add Button component with styles and hover effects

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+	let { children, ...props } = $props();
+</script>
+
+<button type="button" {...props}>{@render children()}</button>
+
+<style>
+	button {
+		background-color: purple;
+		color: white;
+		padding: 20px 24px;
+		font-weight: normal;
+		font-size: 22px;
+		text-transform: uppercase;
+		transition: all 500ms;
+    border: 1px solid white;
+		border-radius: 8px;
+	}
+
+	button:hover {
+		background-color: white;
+		color: black;
+	}
+</style>
