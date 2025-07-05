e5d511a2b94b59937ede67577d972ee5923bdb96
aquacalc <aquacalc@yahoo.com>
2025-07-04 21:36:42 -0400

    Add HeroSection component and update exports in index.js

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,23 @@
+<!-- Serve from static or as alias? sequence... -->
+<!-- https://stackoverflow.com/questions/71789244/where-to-put-images-with-sveltekit -->
+<section class="hero">
+	<div class="hero-showcase bold text-center">
+		<h1 class="purple">MOVE TO</h1>
+		<h1 class="purple mb-xs">SPAIN</h1>
+		<h4 class="semi-bold italic">There's no need to make the same mistake that I made!</h4>
+		<img src="book_cover.png" alt="" class="book-cover" />
+		<img src="phone_cover.png" alt="" class="phone-cover" />
+	</div>
+
+	<!-- Right Side -->
+	<div class="hero-text white text-center" style='background-color: #3233a6'>
+		<h1>This eBook will</h1>
+		<h1>Save you money,</h1>
+		<h1 class="mb-m">time, and nerves!</h1>
+		<p class="light-grey">
+			I moved to Spain seven years ago. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+			Enim doloribus modi dolor beatae recusandae assumenda. Cum, magni. At ullam error dignissimos
+			ad? Iste quos nostrum tenetur commodi eos aperiam accusantium.
+		</p>
+	</div>
+</section>
