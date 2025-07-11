e0b702c6609c7f5afbbdda327836591bbb84c9f9
aquacalc <aquacalc@yahoo.com>
2025-07-11 18:54:01 -0400

    Add AuthorSection and FaqSection components with Sidebar navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
 <script>
-	import { HeroSection, ChapterPreview } from '$lib';
+	import { HeroSection, ChapterPreview, AuthorSection, FaqSection, Sidebar } from '$lib';
 
+	const menuItems = [
+		{ label: 'Home', href: '/' },
+		{ label: 'Chapters', href: '#chapters' },
+		{ label: 'Author', href: '#author' },
+		{ label: 'FAQ', href: '#faq' }
+	];
 </script>
+
+<Sidebar items={menuItems} />
 
 <HeroSection />
 
-<ChapterPreview />
+<div id="chapters">
+	<ChapterPreview />
+</div>
+
+<div id="author">
+	<AuthorSection />
+</div>
+
+<div id="faq"></div>
+<FaqSection />
