8900c4549908d2ccbbee43b3859c69e85bad7af9
aquacalc <aquacalc@yahoo.com>
2025-07-12 10:09:38 -0400

    Add failure and success pages for checkout flow

---
+++ src/routes/checkout/failure/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { HeroSection, Button } from '$lib';
+</script>
+
+<HeroSection>
+	<h1>Cancel Order</h1>
+  <Button class='mt-m'>Try again!</Button>
+</HeroSection>
---
+++ src/routes/checkout/success/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import {HeroSection} from '$lib';
+
+</script>
+
+<HeroSection>
+	<h1>Success</h1>
+</HeroSection>
